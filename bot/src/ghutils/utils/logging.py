ec025881e25e1af5e1e3294f23ebeadf3f3a610a
object-Object <object@objectobject.ca>
2024-09-09 23:20:06 -0400

    Silence httpx request log messages

--- bot/src/ghutils/utils/logging.py
+++ bot/src/ghutils/utils/logging.py
@@ -3,7 +3,9 @@ import logging
 import discord
 
 
-def setup_logging():
+def setup_logging(verbose: bool = False):
     discord.utils.setup_logging()
     logging.getLogger("ghutils").setLevel(logging.DEBUG)
+    if not verbose:
+        logging.getLogger("httpx").setLevel(logging.WARNING)
     discord.VoiceClient.warn_nacl = False
