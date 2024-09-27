fe952b3852c2b8d864292391ba8bb12a718c02ea
object-Object <object@objectobject.ca>
2024-09-26 20:35:03 -0400

    Update to new object-Object/ci and use per-repo CDK role

--- infrastructure/src/ghutils/aws_cdk/app.py
+++ infrastructure/src/ghutils/aws_cdk/app.py
@@ -1,7 +1,6 @@
 import logging
 
 from object_ci.aws_cdk.codedeploy.stack import CodeDeployStack
-from object_ci.aws_cdk.codedeploy.types import GitHubRepository
 from object_ci.logging import setup_logging
 
 import aws_cdk as cdk
@@ -15,11 +14,7 @@ def main():
     logger.info("Ready.")
     app = cdk.App()
 
-    CodeDeployStack.default_prod_stack(
-        app,
-        base_stack_name="discord-github-utils",
-        github_repo=GitHubRepository("object-Object", "discord-github-utils"),
-    )
+    CodeDeployStack.default_prod_stack(app)
 
     logger.info("Synthesizing.")
     app.synth()
