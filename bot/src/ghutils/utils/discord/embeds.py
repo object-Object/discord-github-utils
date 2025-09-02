bd31109342b403e6185dd336880f82ccfd0b8dc3
object-Object <object@objectobject.ca>
2025-09-01 23:37:12 -0400

    Increase description length limit and add max line limit

--- bot/src/ghutils/utils/discord/embeds.py
+++ bot/src/ghutils/utils/discord/embeds.py
@@ -20,18 +20,31 @@ def set_embed_author(embed: Embed, user: SimpleUser):
 
 _NEWLINE_PATTERN = re.compile(r"\n[ \t]*\n([ \t]*\n)+")
 
-_NEWLINE_HEADING_PATTERN = re.compile(r"(\n|^)\n+(#+ +\S)")
+# remove extra newlines before heading, and fix double newline after heading
+_NEWLINE_HEADING_PATTERN = re.compile(r"(\n|^)\n+(#+ +[^\n]+(?:\n|$))\n?")
 
 
 @overload
-def truncate_markdown_description(text: str, limit: int = ...) -> str: ...
+def truncate_markdown_description(
+    text: str,
+    limit: int = ...,
+    line_limit: int | None = ...,
+) -> str: ...
 
 
 @overload
-def truncate_markdown_description(text: str | None, limit: int = ...) -> str | None: ...
+def truncate_markdown_description(
+    text: str | None,
+    limit: int = ...,
+    line_limit: int | None = ...,
+) -> str | None: ...
 
 
-def truncate_markdown_description(text: str | None, limit: int = 256) -> str | None:
+def truncate_markdown_description(
+    text: str | None,
+    limit: int = 512,
+    line_limit: int | None = 16,
+) -> str | None:
     if text is None:
         return None
 
@@ -40,14 +53,26 @@ def truncate_markdown_description(text: str | None, limit: int = 256) -> str | N
     text = _NEWLINE_HEADING_PATTERN.sub(r"\1\2", text)
     text = text.strip()
 
-    if len(text) <= limit:
+    if len(text) <= limit and line_limit is None:
         return text
 
-    # truncate the string at the first whitespace character after overflowing
-    # TODO: would it be better to truncate before instead?
     i = 0
+    newline_count = 0
     for c in text:
+        # truncate the string at the first whitespace character after overflowing
+        # TODO: would it be better to truncate before instead?
         if i >= limit and c.isspace():
             break
+
+        if line_limit is not None and c == "\n":
+            newline_count += 1
+            if newline_count >= line_limit:
+                break
+
         i += 1
+    else:
+        # if we didn't break or run over the limit, don't truncate
+        if i <= limit:
+            return text
+
     return text[:i] + "..."
